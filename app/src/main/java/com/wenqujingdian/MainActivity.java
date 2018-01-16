d31881f94cae2be1abb51d89d72195c53564da05
妥达科技 <17744423526@163.com>
2018-01-17 00:31:54 +0800

    家里的环境配置好了

--- app/src/main/java/com/wenqujingdian/MainActivity.java
+++ app/src/main/java/com/wenqujingdian/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends BaseActivity {
     public void initView() {
         super.initView();
         setContentView(R.layout.activity_home);
+//        解放路卡萨丁
 
     }
 
